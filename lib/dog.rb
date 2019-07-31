119ea2add1f15f30b90e823152a6845d41c1adab
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-31 04:30:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
 
-  def name=(dog_name)
-    @dog_name = dog_name
-  end
+ def name=(dog_name)
+   @new_dog_name = dog_name
+ end
 
-  def name
-    @dog_name
-  end
+ def name
+   @new_dog_name
+ end
 
   def breed=(dog_breed)
     @dog_breed = dog_breed
